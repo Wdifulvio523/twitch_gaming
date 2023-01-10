cd58cc08ad6c74a13aae42df261a2608ee7c336d
sbullocks <sbullocks@gmail.com>
2023-01-10 13:28:26 -0500

    connects to the Twitch chat server and sets up event handlers for the message and connected events.

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js
+const tmi = require('./tmi');
+
+// Define configuration options
+const opts = {
+    identity: {
+        username: 'your_bot_username',
+        password: 'oauth:YOUR_OAUTH_TOKEN'
+    },
+    channels: [
+        'channel_name_to_join'
+    ]
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', require('./onMessageHandler'));
+client.on('connected', require('./onConnectedHandler'));
+
+// Connect to Twitch:
+client.connect();
